1f650a88dc9d35694216b6d0a7de7a1dcd7c72c6
Lio <liobensignor@gmail.com>
2024-11-13 00:14:21 -0300

    Fix Index

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
-    <!-- <HomeHero /> -->
+    <HomeHero />
     <HomeBusinessBenefits />
     <!-- <HomeServices /> -->
-    <HomePlans />
+    <!-- <HomePlans /> -->
   </main>
 </template>
